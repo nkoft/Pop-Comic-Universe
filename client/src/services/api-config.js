0de1881112fc2b7c0c210b8dc43d049fd93bfbe9
Nick Kofteris <nkofteris@yahoo.com>
2021-06-25 14:47:50 -0400

    prep for netlify deployment

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3000";
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://git.heroku.com/pop-comics-api.git"
+    : "http://localhost:3000";
 
 const api = axios.create({
   baseURL: baseUrl,
