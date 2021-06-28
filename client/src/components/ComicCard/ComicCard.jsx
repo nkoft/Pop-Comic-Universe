216148082a4f93e3b80d3181d20b873a8df51efc
Nick Kofteris <nkofteris@yahoo.com>
2021-06-28 11:57:32 -0400

    comic card div fields

--- client/src/components/ComicCard/ComicCard.jsx
+++ client/src/components/ComicCard/ComicCard.jsx
@@ -1,7 +1,15 @@
 import "./ComicCard.css";
+import { Link } from "react-router-dom";
 
 const ComicCard = (props) => {
-  return <div className="comic-card"></div>;
+  return (
+    <div className="comic-card">
+      <Link className="card" to={`/comics/${props._id}`}>
+        <img className="comic-card-image" src={props.image_url} />
+      </Link>
+      <p className="comic-card-title">{props.title}</p>
+    </div>
+  );
 };
 
 export default ComicCard;
