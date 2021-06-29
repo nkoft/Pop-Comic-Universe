20500dde5a8f8e32b7b98108545b72ca81341fcb
Nick Kofteris <nkofteris@yahoo.com>
2021-06-28 22:04:54 -0400

    reviews component done

--- client/src/components/Reviews/Reviews.jsx
+++ client/src/components/Reviews/Reviews.jsx
@@ -1,17 +1,21 @@
 import "./Reviews.css";
+import { useEffect, useState } from "react";
 
 const Reviews = (props) => {
-  const { reviews } = props;
-  return (
-    reviews && (
-      <div className="reviews">
-        <h3>Reviews:</h3>
+  const [reviews, setReviews] = useState([]);
 
-        {reviews.map((item, index) => (
-          <div></div>
-        ))}
-      </div>
-    )
+  useEffect(() => {
+    console.log(props.reviews);
+    setReviews(props.reviews || []);
+  }, [props.reviews]);
+
+  return (
+    <div>
+      <h3>Reviews:</h3>
+      {reviews.map((item, index) => (
+        <div key={index}>{item.post}</div>
+      ))}
+    </div>
   );
 };
 
